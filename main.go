50e236fe4683d44238e2e2d066bf19f78e3f388d
johnsuresh4 <johnsuresh777@gmail.com>
2026-04-13 13:28:01 +0530

    Section 1 Getting Started

--- main.go
+++ main.go
@@ -1,39 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-type Person struct {
-	name string
-	age  int
-	f    func(string)
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
 }
-
-func getName(p Person) string {
-	return p.name
-}
-
-// func handleFunc(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
-// }
 
 func main() {
-	p1 := Person{age: 23}
-	p1.name = "Suresh"
-	p1.age = 34
-	p1.f = func(s string) {
-		fmt.Println(s + "s")
-	}
-	p1.f("Hello")
-	p2 := Person{age: 24}
-	p2.name = "Suresh"
-	p2.age = 34
-	p2.f = func(s string) {
-		fmt.Println(s + "asd")
-	}
-	p2.f("Hello")
+	http.HandleFunc("/", handleFunc)
+	fmt.Println("Starting the server: 3000")
+	http.ListenAndServe(":3000", nil)
 }
-
-// func helloUser(w http.ResponseWriter, r *http.Request) {
-// 	var greeting = "Hello user. Welcome to our Todolist Application"
-// 	fmt.Fprintln(w, greeting)
-// }

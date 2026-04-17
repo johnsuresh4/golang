016c581a35139a552d022d78294cd5d5e7a90fb2
johnsuresh4 <johnsuresh777@gmail.com>
2026-04-18 01:20:31 +0530

    Section 3: Routers and 3rd Party Libraries CHI

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/johnsuresh4/golang_web
+
+go 1.25.8
+
+require github.com/go-chi/chi/v5 v5.2.5 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,51 +23,13 @@ func noFoundHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Page Not Found</h1>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	// if r.URL.Path == "/" {
-	// 	homeHandler(w, r)
-	// 	return
-	// } else if r.URL.Path == "/contact" {
-	// 	contactHandler(w, r)
-	// 	return
-	// }
-
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		// TODO: handle the page not found error
-		noFoundHandler(w, r)
-	}
-}
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		// TODO: handle the page not found error
-// 		noFoundHandler(w, r)
-// 	}
-// }
-
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
-
-	// http.Handler - interface with ServeHTTP Method
-
-	// http.HandlerFunc - a function type that accepts same args as ServeHTTP method, also implements http.Handler
-
-	// http.H
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(noFoundHandler)
 
 	fmt.Println("Starting the server: 3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 
 }

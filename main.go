878840c53cd8b9ff75527a6da5b281b3be53a345
johnsuresh4 <johnsuresh777@gmail.com>
2026-04-26 09:30:08 -0300

    Added template

--- main.go
+++ main.go
@@ -3,34 +3,60 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("hello.gohtml")
+func executeTemplate(w http.ResponseWriter, filepath string, user User) {
 	w.Header().Set("content-type", "text/html; charset=utf-8")
-
+	t, err := template.ParseFiles("hello.gohtml")
 	if err != nil {
-		panic(err)
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
 	}
 
-	user := User{
-		Name: "John Suresh",
-		Age:  32,
-		Meta: UserMeta{
-			Visits: 8,
-		},
+	err = t.Execute(w, user)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
 	}
 
-	error := t.Execute(w, user)
-
-	if error != nil {
-		panic(error)
-	}
-	// fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
 }
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// user := User{
+	// 	Name: "John Suresh",
+	// 	Age:  32,
+	// 	Meta: UserMeta{
+	// 		Visits: 8,
+	// 	},
+	// }
+	// executeTemplate(w, "hello.gohtml", user)
+
+	// bio := `&lt;script&gt;alert(&quot;Hi!&quot;);&lt;/script&gt;`
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tp1, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tp1.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+// fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:johnsuresh777@gmail.com\">johnsuresh777@gmail.com</a></p>")
